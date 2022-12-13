f69e4e1953ad354b34c4d4d4cdf99f41b9f5be8d
智能大石头 <nnhy@vip.qq.com>
2022-12-13 22:28:34 +0800

    增加地图设置页面

---
+++ MapApi/Areas/Location/Controllers/MapController.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using IoTWeb;
+using MapApi.Areas.Location;
+using NewLife.Cube;
+
+namespace MapApi.Areas.Location.Controllers;
+
+/// <summary>地图设置控制器</summary>
+[DisplayName("地图设置")]
+[LocationArea]
+public class MapController : ConfigController<MapSetting>
+{
+}
